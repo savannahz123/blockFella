bcf341b89a4c938000215fe0d1b6ed6acab670ac
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-08-11 17:12:51 -0700

    fundamental MERN stack code architecture done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      Hello, reactJS!
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    apiData: []
+  }
+
+  // a demo function to get data from server api
+  async getDataFromAPI() {
+    console.log("scraping...");
+    
+    await axios.get("http://localhost:3001/api/scrape")
+    .then((res) => (this.setState({apiData : res})))
+    .catch(function(err) {
+      console.log(err);
+    })
+
+    console.log(this.state);
+  }
+
+  // call API when the component is mounted
+  componentDidMount() {
+    this.getDataFromAPI();
+  }
+
+  render() {
+    // get the API data for rendering
+    let dataList = this.state.apiData.data;
+    return (
+      <div>
+        hello, reactJS!
+        {
+          JSON.stringify(dataList)    // stringify the API JSON data
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
